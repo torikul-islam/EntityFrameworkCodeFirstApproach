0b13e18642ddc770d1de01b6cb43dd02d283dede
unknown <torikulislam024@gmail.com>
2018-06-07 09:53:45 +0600

    Fitst commit

---
+++ BridgeTableInManyToManyRelationship/Home.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BridgeTableInManyToManyRelationship
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            StudentCourseDBContex studentCourseDBContex = new StudentCourseDBContex();
+            var query = from course in studentCourseDBContex.Courses
+                        from studentName in course.StudentCourses
+                        select new
+                        {
+                            StudentName = studentName.Student.StudentName,
+                            CourseName = course.CourseName,
+                            EnrollDates = studentName.EnrolledDate
+
+                        };
+            GridView1.DataSource = query.ToList();
+            GridView1.DataBind();
+        }
+    }
+}
